16247190a9f0ff5e66a4b8b25f3896b05d66d244
isfar-uddin <isfarruett@gmail.com>
2019-09-28 17:38:24 +0600

    Configure firebase and add css for chat room
    - Create new project in firebase
    - Create config const according to the configuration of the project
    - Style the chat room page

--- src/Component/Room.js
+++ src/Component/Room.js
@@ -1,11 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class Room extends Component {
-
+class Room extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentUser: props.currentUser,
+            currentUser: { name: "Me" },
             messages: []
         };
     }
@@ -13,41 +12,37 @@ export default class Room extends Component {
     sendMessage(e) {
         e.preventDefault();
 
-        const user = this.state.currentUser;
-        const text = e.target.message.value.trim();
-        const time = new Date().getTime();
-
-        // set the new object value
-        const message= {
-            id: Math.random().toString(36).slice(2,13),
-            user:user,
-            text:text,
-            time:time
+        // construct the new message
+        const message = {
+            id: Math.random()
+                .toString(36)
+                .slice(2, 13), // a quick a way to generate random string
+            user: this.state.currentUser,
+            time: new Date(),
+            text: e.target.message.value.trim()
         };
 
-        this.setState({
-            messages: [...this.state.messages,message]
-        });
-        e.target.message.value = '';
+        // update messages without mutating
+        this.setState({ messages: [...this.state.messages, message] });
+
+        // clear the input field
+        e.target.message.value = "";
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="container">
                 <div className="messages">
                     {this.state.messages.map(m => (
                         <div key={m.id} className="message">
-                            <img className="avatar" src={m.user.photo} />
                             <div className="message-info">
-                                {m.user.name},{' '}
-                                {/* we are not storing date object in state anymore.
-                 So, have to create a date object */}
-                                {new Date(m.time).toLocaleDateString('en-US', {
-                                    hour: 'numeric',
-                                    minute: 'numeric',
-                                    day: 'numeric',
-                                    month: 'short',
-                                    year: 'numeric'
+                                {m.user.name},
+                                {m.time.toLocaleDateString("en-US", {
+                                    hour: "numeric",
+                                    minute: "numeric",
+                                    day: "numeric",
+                                    month: "short",
+                                    year: "numeric"
                                 })}
                             </div>
                             <div className="message-text">{m.text}</div>
@@ -55,14 +50,11 @@ export default class Room extends Component {
                     ))}
                 </div>
                 <form className="send-form" onSubmit={e => this.sendMessage(e)}>
-                    <input
-                        placeholder="Type and press enter to send..."
-                        className="message-field"
-                        name="message"
-                        type="text"
-                    />
+                    <input className="message-field" name="message" type="text" />
                 </form>
             </div>
-        )
+        );
     }
 }
+
+export default Room;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app';
+import database from 'firebase/database';
+
+const config = {
+    apiKey : 'AIzaSyDuXUQuwKexaAk9ZfBAhUH-OsGW4KBdNVM',
+    authDomain:'chat-room-ee9e6.firebaseapp.com',
+    databaseUrl :'https://chat-room-ee9e6.firebaseio.com/',
+    projectId:'chat-room-ee9e6',
+    storageBucket:'chat-room-ee9e6.appspot.com',
+    messagingSenderId:'540825649704'
+};
